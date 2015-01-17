860388c2b725a0add2f7bf98ebd91d4bca333823
Nishil Shah <nishil.shah17@gmail.com>
2015-01-17 17:04:01 -0500

    bicep curl updated

--- MyoFit/src/com/bran/android/BicepCurl.java
+++ MyoFit/src/com/bran/android/BicepCurl.java
@@ -6,31 +6,41 @@ import com.thalmic.myo.Myo;
 import com.thalmic.myo.Vector3;
 
 public class BicepCurl extends Exercise {
-	
+
+	private boolean down = true;
+	private double minAngle = 1.2217; //70 degrees
+	private double downAngle = 0.1745; //10 degrees
+
 	public BicepCurl() {
 		super("Bicep Curl",ExerciseType.BICEP_CURL);
-		
+
 	}
-	
+
 	@Override
 	public void processData(Myo myo, long timestamp, Vector3 vector, DataType type) {
-		
+
 		// Process Data
-		
-		if (type.equals(DataType.ACCELEROMETER)) {
-			
-			Log.i("BicepCurl", "BicepCurl - Accelerometer x: "+vector.x());
-			
-			if (vector.x() > 1) {
-				rep++;
-			}
-			
-		} else if (type.equals(DataType.GYROSCOPE)) {
-			
-			
-			
+		if(type.equals(DataType.GYROSCOPE)) {
+
+			Log.i("BicepCurl Gyroscope","z: "+vector.z());
+			if(started) {
+				if(down && vector.z() > minAngle) {
+					down = false;
+					reps++;
+				} else if (!down && vector.z() < downAngle)
+					down = true;
+				}
+		  }
 		}
-		
+
 	}
-	
+
+}
+
+if(down && myo.getYaw() > minAngle) {
+	down = false;
+	reps++;
+}
+if(down = false && myo.getYaw() < downAngle) {
+	down = true;
 }
