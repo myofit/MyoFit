ca4d132f02b17a6a8e8ae7672049e8bfd43d6eac
RamV13 <ramsai.vellanki@gmail.com>
2015-01-17 19:30:31 -0500

    Myo Orientation Testing

--- MyoFit/src/com/bran/android/BicepCurl.java
+++ MyoFit/src/com/bran/android/BicepCurl.java
@@ -6,30 +6,40 @@ import com.thalmic.myo.Myo;
 import com.thalmic.myo.Myo.VibrationType;
 import com.thalmic.myo.Quaternion;
 import com.thalmic.myo.Vector3;
+import com.thalmic.myo.XDirection;
 
 public class BicepCurl extends Exercise {
 
 	private boolean down = true;
-	private double minAngle = 1.2217; //70 degrees
-	private double downAngle = -0.1745; //-10 degrees
+	private double minAngle = .25; //70 degrees
+	private double downAngle = -0.2; //-10 degrees
+	
+	private int direction;
 
 	public BicepCurl() {
 		super("Bicep Curl",ExerciseType.BICEP_CURL);
+		
+		direction = 1;
+		
 	}
 
 	@Override
 	public void processData(Myo myo, long timestamp, Quaternion quaternion, DataType type) {
 
+		if (myo.getXDirection().equals(XDirection.TOWARD_ELBOW))
+			direction = -1;
 
 		// Process Data
 		if(type.equals(DataType.ORIENTATION)) {
-			Log.i("BicepCurl Angles","z: "+quaternion.z());
+			Log.i("BicepCurl Angles","x: "+direction*quaternion.x());
+			Log.i("BicepCurl Angles","y: "+direction*quaternion.y());
+			Log.i("BicepCurl Angles","z: "+direction*quaternion.z());
 			if(started) {
-				if(down && quaternion.z() > minAngle) {
+				if(down && direction*quaternion.z() > minAngle) {
 					down = false;
 					rep++;
 					myo.vibrate(VibrationType.SHORT);
-				} else if (!down && quaternion.z() < downAngle) {
+				} else if (!down && direction*quaternion.z() < downAngle) {
 					down = true;
 				}
 			}
